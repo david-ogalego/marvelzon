32355991ef532408443bf6e5feeb8275a9630dd7
David Gómez <david.gomez@beezy.net>
2017-11-17 20:58:34 +0100

    Convert date correctly with moment

--- src/app/client/components/ComicItem/ComicItem.js
+++ src/app/client/components/ComicItem/ComicItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import stylesComicItem from './ComicItem.css';
+import moment from 'moment';
 
 const ComicItem = function(props) {
+    const dateComic = moment(props.date).format("YYYY-MM-DD HH:mm:ss");
     return (
         <Link className={stylesComicItem.item} key={props.videoId} to={`/detail/${props.id}`} >
             <div className={stylesComicItem.containerImage} >
                 <img className={stylesComicItem.image} alt={props.title} src={props.image} /> 
                 <div className={stylesComicItem.details}>
                     <h3>{props.title}</h3>
-                    Date: <label>{props.date}</label>
+                    Date: <label>{dateComic}</label>
                     Price: <label>{props.price}</label>
                 </div>
             </div>
