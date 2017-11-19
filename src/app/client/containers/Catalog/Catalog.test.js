084c8c241942762a0603c79a77f5ed834be79e51
David Gómez <david.gomez@beezy.net>
2017-11-19 19:12:10 +0100

    Add test to Catalog container

---
+++ src/app/client/containers/Catalog/Catalog.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import { mount } from 'enzyme';
+import Catalog from './Catalog';
+import { createMockStore } from 'redux-test-utils';
+import shallowWithStore from '../../helpers/shallowWithStore';
+import { RESET_COMIC, REQUEST_COMICS } from '../../redux/actions/actionTypes';
+
+test('Catalog dispatch actions', () => {
+    const state = {
+        catalogReducer: {
+            limitComics: 20,
+            offsetComics: 0,
+            comics: [],
+            loadingCatalog: false,
+        }
+    };
+    const store = createMockStore(state);
+    mount(<Provider store={store}><Catalog /></Provider> )
+    expect(store.isActionDispatched({
+        type: RESET_COMIC
+    })).toBe(true);
+    // expect(store.isActionDispatched({
+    //     type: REQUEST_COMICS,
+    //     loadingCatalog: true,
+    //     limitComics: 20,
+    //     offsetComics: 0
+    // })).toBe(true);
+});
