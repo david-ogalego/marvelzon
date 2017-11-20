f0418287fa6fac37bf715f9e1d81880c2eeb26d0
David Gómez <david.gomez@beezy.net>
2017-11-20 23:44:11 +0100

    Remove comments

--- src/app/client/App.js
+++ src/app/client/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-// import { BrowserRouter, Switch, Route } from 'react-router-dom';
-// import { Catalog, ComicDetailContainer, NavContainer } from './containers/';
 import routes from './routes';
 import configureStore from './redux/store/configureStore';
 import stylesApp from './App.css';
